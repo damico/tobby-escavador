3e5b3d873df0a4ce5053bccbe8d3bc0cc0c623d4
José Damico <jd.comment@gmail.com>
2015-08-10 17:56:22 -0300

    first upload

---
+++ eclipse-project/tobby-escavador/src/com/scicrop/tobbyescavador/tests/TestLoc.java
@@ -0,0 +1,173 @@
+package com.scicrop.tobbyescavador.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class TestLoc {
+
+	@Test
+	public void test() {
+
+		List<Location> locLst = new ArrayList<TestLoc.Location>();
+		//		locLst.add(new Location(-48.705, -25.131));
+		//	
+		//		locLst.add(new Location(-48.440, -25.051));
+		//		
+		//		locLst.add(new Location(-48.520, -25.290));
+		//		
+		//		locLst.add(new Location(-48.767, -25.290));
+		//
+		//
+		//		
+		//	//	locLst.add(new Location(1, 1));
+		//	//	locLst.add(new Location(1, 3));
+		//	//	locLst.add(new Location(2, 3));
+		//	//	locLst.add(new Location(3, 2));
+		//	//	locLst.add(new Location(2, 1));
+		//		
+		//	//	Location locKey = new Location(2, 4);
+		//		
+		//		
+		//		//Location locKey = new Location(-48.652,  -25.131);
+		//		
+		//		Location locKey = new Location(-49.377,  -24.167);
+
+		Location locKey = new Location(-47.848,  -24.839);
+
+
+
+		locLst.add(new Location(-48.00924161435284532,-25.0298111948637505));
+		locLst.add(new Location(-48.01507309706189375,-25.05032332761048153));
+		locLst.add(new Location(-48.02162092463886722,-25.05825128242726763));
+		locLst.add(new Location(-48.03137591921485239,-25.06336318838589605));
+		locLst.add(new Location(-48.05354555732795774,-25.07142727625642209));
+		locLst.add(new Location(-48.09595835684928034,-25.07825079110750366));
+		locLst.add(new Location(-48.12890262146211739,-25.07537369874270183));
+		locLst.add(new Location(-48.13909482759208203,-25.07360581699360225));
+		locLst.add(new Location(-48.14942970114910281,-25.06954583822396643));
+		locLst.add(new Location(-48.15753880373599571,-25.06077234944990906));
+		locLst.add(new Location(-48.1563159346851819,-25.04002011028958563));
+		locLst.add(new Location(-48.132927084080535,-25.01121855739187438));
+		locLst.add(new Location(-48.09477357514872864,-24.97704768129894859));
+		locLst.add(new Location(-48.04172935020476842,-24.9397894296585072));
+		locLst.add(new Location(-48.00332801148705641,-24.91018534369260706));
+		locLst.add(new Location(-47.98398851109337926,-24.89767155848423741));
+		locLst.add(new Location(-47.97955513543555384,-24.88824356711342034));
+		locLst.add(new Location(-47.97246843163237884,-24.88097296323066132));
+		locLst.add(new Location(-47.95548444199898341,-24.85134379058849419));
+		locLst.add(new Location(-47.94381169678477761,-24.81740020288152948));
+		locLst.add(new Location(-47.9337851931001282,-24.81686776108155001));
+		locLst.add(new Location(-47.92095977298826881,-24.82078168205458724));
+		locLst.add(new Location(-47.90842567973624,-24.82011449916016588));
+		locLst.add(new Location(-47.86684521625767275,-24.80180540194688277));
+		locLst.add(new Location(-47.80936540293470927,-24.75735347374432749));
+		locLst.add(new Location(-47.79964068725556814,-24.75223333008257143));
+		locLst.add(new Location(-47.78299513578766522,-24.73754693372206859));
+		locLst.add(new Location(-47.77091746491778679,-24.72999988299028828));
+		locLst.add(new Location(-47.76384874073823283,-24.72272430813049837));
+		locLst.add(new Location(-47.75943514956230729,-24.7132941316764132));
+		locLst.add(new Location(-47.75915005954332315,-24.6983298752129663));
+		locLst.add(new Location(-47.75945847106584097,-24.67423275786144998));
+		locLst.add(new Location(-47.75754919867844706,-24.66493878651905192));
+		locLst.add(new Location(-47.74904159724851382,-24.65834829572379405));
+		locLst.add(new Location(-47.74533554683198844,-24.66120730064065825));
+		locLst.add(new Location(-47.73058903329837932,-24.66100453009340754));
+		locLst.add(new Location(-47.70868845309866657,-24.64730426094870808));
+		locLst.add(new Location(-47.69575845828853033,-24.64879802258832697));
+		locLst.add(new Location(-47.68590466766210767,-24.6871742924144506));
+		locLst.add(new Location(-47.67093098663415418,-24.70035811328652287));
+		locLst.add(new Location(-47.65439294587816477,-24.69677201407807843));
+		locLst.add(new Location(-47.62895086553869106,-24.67463756674635889));
+		locLst.add(new Location(-47.59372184280447726,-24.68584377923382434));
+		locLst.add(new Location(-47.585739066681775,-24.68746451361794314));
+		locLst.add(new Location(-47.56918553498076818,-24.69651542054936755));
+		locLst.add(new Location(-47.55543139892478877,-24.70324469939959755));
+		locLst.add(new Location(-47.54990658999653164,-24.71281615396580378));
+		locLst.add(new Location(-47.60938930898501553,-24.73965266636151128));
+		locLst.add(new Location(-47.81553785541099444,-24.88658248979482934));
+		locLst.add(new Location(-47.89830449995456974,-24.90945808141173501));
+		locLst.add(new Location(-47.92195949776127861,-24.93321617697513659));
+		locLst.add(new Location(-47.95670935296847404,-24.95711394382329118));
+		locLst.add(new Location(-47.98203277678297241,-24.99428931702341927));
+		locLst.add(new Location(-47.9988890255989773,-24.97942323424226174));
+		locLst.add(new Location(-48.00812910350984453,-24.97953855384838917));
+		locLst.add(new Location(-48.01345348114784883,-24.9946820871555957));
+		locLst.add(new Location(-48.00924161435284532,-25.0298111948637505));
+
+
+		System.out.println(contains(locKey, locLst));
+	}
+
+	public boolean contains(Location locKey, List<Location> locLst) {
+
+
+		Location lastPoint = locLst.get(locLst.size() -1);
+
+
+		boolean isInside = false;
+		double x = locKey.getLon();
+
+		for (Location point : locLst) {
+
+			double x1 = lastPoint.getLon();
+			double x2 = point.getLon();
+			double dx = x2 - x1;
+
+
+			if (Math.abs(dx) > 180.0)
+			{
+				if (x > 0) {
+					while (x1 < 0)
+						x1 += 360;
+					while (x2 < 0)
+						x2 += 360;
+				} else {
+					while (x1 > 0) x1 -= 360;
+					while (x2 > 0) x2 -= 360;
+				}
+				dx = x2 - x1;
+			}
+
+			if ((x1 <= x && x2 > x) || (x1 >= x && x2 < x)) {
+				
+				double grad = (point.getLat() - lastPoint.getLat()) / dx;
+				double intersectAtLat = lastPoint.getLat() + ((x - x1) * grad);
+
+				if (intersectAtLat > locKey.getLat()) isInside = !isInside;
+			}
+			lastPoint = point;
+		}
+
+		return isInside;
+	}
+
+
+	public class Location{
+
+		private double lat = .0;
+		private double lon = .0;
+
+		public double getLat() {
+			return lat;
+		}
+		public void setLat(double lat) {
+			this.lat = lat;
+		}
+		public double getLon() {
+			return lon;
+		}
+		public void setLon(double lon) {
+			this.lon = lon;
+		}
+		public Location(double lat, double lon) {
+			super();
+			this.lat = lat;
+			this.lon = lon;
+		}
+
+	}
+}
